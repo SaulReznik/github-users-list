6ceb7f2974a381e14399f4588964a3eb3a47df88
SaulReznik <robhov97@gmail.com>
2020-07-07 21:06:35 +0400

    Updates

---
+++ precache-manifest.5511b817ccb98321fdfe584d72123688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d50a65c8561347a72cd8d8a6334fca",
+    "url": "/github-users-list/index.html"
+  },
+  {
+    "revision": "2344ae0a41ea2116f8e2",
+    "url": "/github-users-list/static/css/main.0ae51a10.chunk.css"
+  },
+  {
+    "revision": "8703ce43fb8698da30b3",
+    "url": "/github-users-list/static/js/2.e5e26c33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-users-list/static/js/2.e5e26c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2344ae0a41ea2116f8e2",
+    "url": "/github-users-list/static/js/main.569d02fb.chunk.js"
+  },
+  {
+    "revision": "a1f79398e0fbe3ee539b",
+    "url": "/github-users-list/static/js/runtime-main.c552a6bf.js"
+  }
+]);
